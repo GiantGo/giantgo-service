f797d6bd37b367e1feea2b50a9b40f752fd334c2
spx <spx123456@foxmail.com>
2023-04-07 14:57:35 +0800

    feat: api auth/mine

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,10 +5,13 @@ import {
   HttpCode,
   HttpStatus,
   UseGuards,
+  Get,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local.guard';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { User } from '../users/entities/user.entity';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -20,5 +23,12 @@ export class AuthController {
   @Post('token')
   signIn(@Request() req: any): Promise<{ token: string }> {
     return this.authService.signIn(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('mine')
+  me(@Request() req: any): Promise<User> {
+    console.log(req.user);
+    return req.user;
   }
 }
--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -14,6 +14,10 @@ import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import {
+  PermissionsGuard,
+  Permissions,
+} from 'src/auth/guards/permission.guard';
 
 @ApiBearerAuth()
 @ApiTags('permissions')
@@ -21,27 +25,37 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_READ']])
   @Get('tree')
   getPermissionTree() {
     return this.permissionsService.getPermissionTree();
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_READ']])
   @Get()
   getPermissionList() {
     return this.permissionsService.getPermissionList();
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_READ']])
   @Get(':id')
   getPermission(@Param('id') id: string) {
     return this.permissionsService.getPermission(id);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_CREATE']])
   @UseGuards(JwtAuthGuard)
   @Post()
   async createPermission(@Body() createPermissionDto: CreatePermissionDto) {
     return this.permissionsService.createPermission(createPermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_UPDATE']])
   @Patch(':id')
   updatePermission(
     @Param('id') id: string,
@@ -50,6 +64,8 @@ export class PermissionsController {
     return this.permissionsService.updatePermission(id, updatePermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions([['ADMIN'], ['PERMISSION'], ['PERMISSION_DELETE']])
   @HttpCode(204)
   @Delete(':id')
   deletePermission(@Param('id') id: string) {
